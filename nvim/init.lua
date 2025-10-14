781a507d397862a0f24d5e0a4adb1ad86c227d41
Gabriel Salazar <jesus-gabriel.salazar@artefact.com>
2025-10-14 17:50:00 -0500

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- Basic options
+vim.opt.number = true        -- Show line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.expandtab = true     -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4       -- Number of spaces for indentation
+vim.opt.tabstop = 4          -- Number of spaces per tab
+
+-- Colorscheme
+vim.o.background = "light"
+vim.cmd('colorscheme tatami')
+
+-- 80 columns
+vim.opt.colorcolumn = "80"
+
